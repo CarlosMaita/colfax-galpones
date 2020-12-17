b13544d79e757218adf94a71facd407e08ed273e
Carlos Maita <carlosmaita2009@gmail.com>
2020-12-17 17:13:30 -0400

    ajustes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
- 
+    @yield('metas')
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
