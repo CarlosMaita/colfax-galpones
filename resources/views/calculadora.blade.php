effed8de393a3757860bb189aeedcf1e22a8fb1e
AndresG <gazuiandres@gmail.com>
2020-12-23 15:42:19 -0400

    cambios calculadora

--- resources/views/calculadora.blade.php
+++ resources/views/calculadora.blade.php
@@ -3,7 +3,7 @@
 @section('title')
 Calculadora - galpones     
 @endsection
-
+<link rel="stylesheet" type="text/css" href="{{asset('vendor/chartjs/Chart.min.css')}}">
 
   
 {{-- location  --}}
@@ -23,6 +23,43 @@ Calculadora - galpones
 
   .option_advance.hide {
     display: none;
+  }
+
+  i{
+    cursor: pointer;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  .dropdown-item {
+    display: flex;
+    align-items: center;
+  }
+
+  .share-icon{
+    position: relative;
+    top: -1px;
+    font-size: 1.6rem;
+    margin-right: 1rem;
+  }
+
+  .fa-facebook-square{
+    color: #3B5998;
+  }
+
+  .fa-twitter-square {
+    color: #00ACEE;
+  }
+
+  .fa-linkedin {
+    color: #0E76A8;
+  }
+
+  .chart{
+    width: 100%;
+    height: 100%;
   }
 </style>
 <!-- BREADCRUMB
@@ -76,7 +113,9 @@ Calculadora - galpones
    			<div class="form-group">
    				<h6>Loan program</h6>
    				<select class="form-control">
-   					<option></option>
+            <option>30-year fixed</option>
+            <option>15-year fixed</option>
+   					<option>5/1 ARM</option>
    				</select>
    			</div>
    			<div class="form-group">
@@ -89,14 +128,22 @@ Calculadora - galpones
         <div class="option_advance hide" id="advance">
           <div class="form-check">
             <input type="checkbox" class="form-check-input" name="">
-            <label class="form-check-label">Include PMI</label>
+            <label class="form-check-label">
+              Include PMI 
+              <i class="fas fa-question-circle" title="Private Mortgage Insurance
+Mortgage insurance is required primarily for borrowers with a down payment of less than 20% of the home's purchase price. It protects lenders against some or most of the losses that can occur when a borrower defaults on a mortgage loan."></i>
+            </label>
           </div>
           <div class="form-check">
             <input type="checkbox" class="form-check-input" name="">
             <label class="form-check-label">Include taxes/insurange</label>
           </div>
           <div class="form-group">
-            <h6>Property tax</h6>
+            <h6>
+              Property tax 
+              <i class="fas fa-question-circle" title="Property Tax
+The mortgage payment calculator includes estimated property taxes. The value represents an annual tax on homeowners' property and the tax amount is based on the home's value. To convert an annual property tax in dollars to a percentage, divide the property tax by your home value and then multiply by 100."></i>
+            </h6>
             <div class="d-flex">
               <div>
                 <input class="form-control" type="number" name="">
@@ -107,11 +154,19 @@ Calculadora - galpones
             </div>
           </div>
           <div class="form-group">
-            <h6>Home isurance</h6>
+            <h6>
+              Home isurance 
+              <i class="fas fa-question-circle" title="Homeowners Insurance
+Commonly known as hazard insurance, most lenders require insurance to provide damage protection for your home and personal property from a variety of events, including fire, lightning, burglary, vandalism, storms, explosions, and more. All homeowner's insurance policies contain personal liability coverage, which protects against lawsuits involving injuries that occur on and off your property."></i>
+            </h6>
             <input class="form-control" type="number" name="">
           </div>
           <div class="form-group">
-            <h6>HOA dues</h6>
+            <h6>
+              HOA dues 
+              <i class="fas fa-question-circle" title="Homeowners Association Dues
+Typically, owners of condos or townhomes are required to pay homeowners association dues (known as HOA fees), to cover common amenities or services within the property such as garbage collection, landscaping, snow removal, pool maintenance, and hazard insurance."></i>
+            </h6>
             <input class="form-control" type="number" name="">
           </div>
           <div class="form-group">
@@ -119,8 +174,47 @@ Calculadora - galpones
           </div>
         </div>
    		</div>
-   		<div class="col-lg-6">
-   			
+   		<div class="col-md-7">
+   			<div class="d-flex justify-content-between align-items-center">
+          <ul class=" p-0 m-0 d-flex justify-content-around align-items-center">
+            <li class="mr-4">
+              <a href="#" class=" chart_button" data-chart="donutchart">Breakdonw</a>
+            </li>
+            <li class="mr-4">
+              <a href="#" class=" chart_button" data-chart="linechart" >Schedule</a>
+            </li>
+            <li>
+              <a href="#">Full Report</a>
+            </li>
+          </ul>
+          <div class="dropdown">
+            <button class="btn btn-sm btn-outline-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              share
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">
+                <i class="fab share-icon fa-facebook-square"></i>
+                Facebook
+              </a>
+              <a class="dropdown-item" href="#">
+                <i class="fab share-icon fa-twitter-square"></i>
+                Twitter
+              </a>
+              <a class="dropdown-item" href="#">
+                <i class="fab share-icon fa-linkedin"></i>
+                Linkedin
+              </a>
+              <a class="dropdown-item" href="#">
+                <i class="fas share-icon fa-envelope-square"></i>
+                Email
+              </a>
+            </div>
+          </div>  
+        </div>
+        <hr>
+        <div>
+          <canvas id="myChart" class="chart"></canvas>
+        </div>
    		</div>
    	</div>
    </section>
@@ -332,7 +426,62 @@ Calculadora - galpones
      </div> <!-- / .container -->
    </section>
 
+   <script type="text/javascript" src="{{asset('vendor/chartjs/Chart.min.js')}}"></script>
    <script type="text/javascript">
+
+      function donutChart(container){
+
+        let myDoughnutChart = new Chart(container, {
+            type: 'doughnut',
+            data: {
+              datasets: [{
+                data: [10, 20, 30],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ],
+              }],
+              labels: [
+                  'Red',
+                  'Yellow',
+                  'Blue'
+              ],
+            },
+        });
+      }
+
+      function lineChart(container){
+
+        let linechart = new Chart(container, {
+            type: 'line',
+            data: {
+              datasets: [{
+                data: [0, 59, 75, 20, 25],
+                label: 'speed',
+                borderColor: 'orange',
+                fill: false,
+              }],
+              labels: [
+                  '0s',
+                  '50s',
+                  '70s',
+                  '30s',
+                  '60s'
+              ],
+            },
+        });
+      }
+
+     function makeChart(type){
+        let ctx = document.getElementById('myChart').getContext('2d');
+        
+        if(type === 'donutchart'){
+          donutChart(ctx)
+        }else if(type === 'linechart'){
+          lineChart(ctx)
+        }
+      }
 
      function init(){
 
@@ -345,6 +494,7 @@ Calculadora - galpones
       const advancedOption = document.getElementById('advanced_option')
             simpleOption   = document.getElementById('simple_option')
             menuAdvance    = document.getElementById('advance')
+            chartButtons   = document.querySelectorAll('.chart_button')
 
       advancedOption.addEventListener('click', e => {
         e.preventDefault()
@@ -355,10 +505,18 @@ Calculadora - galpones
         e.preventDefault()
         showAdvanceMenu()
       })
+
+      chartButtons.forEach(button => {
+        button.addEventListener('click', (e) => {
+          e.preventDefault()
+          makeChart(e.target.dataset.chart)
+        })
+      })
      }  
 
      document.addEventListener('DOMContentLoaded', () => {
       init()
+      makeChart('donutchart')
      })
    </script>
 @endsection
