79f9e26f91dfa00646f330d0946fd4407b6746a4
Andrés Gazui <gazuiandres@gmail.com>
2021-01-11 16:11:11 -0400

    floorplants primera vista

--- resources/views/floorplants.blade.php
+++ resources/views/floorplants.blade.php
@@ -21,6 +21,36 @@
 <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
 @endsection
 
+<style>
+  .floorplants__title {
+    margin-top: 7rem;
+    margin-bottom: 4rem;
+  }
+
+  .floorplants__menu {
+    border-left: 1px solid black;
+    padding: 1rem 1rem 0;
+    transform: translateX(5%);
+  }
+
+  .floorplants__optionMain {
+    /* transform: translateY(-20%); */
+  }
+
+  .floorplant__optionIcon {
+    position: relative;
+    transform: translateY(-7%);
+  }
+
+  /* .floorplant__principalImage-background {
+    background-color: #7B7B7B;
+    width: 100%;
+    height: 100%;
+  } */
+  
+</style>
+
+
 {{-- location  --}}
 @section('breadcrumb')
 
@@ -56,10 +86,56 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-           <h1>Floorplants</h1>
+    <h1 class="col-md-12 text-center floorplants__title" >Look at the plans</h1>
+
+    <section class="row">
+      <div class="col-md-5">
+        <h5 class="mb-5">Choose the plane you want to see</h5>
+        <div class=" floorplants__menu">
+          <div class="d-flex justify-content-between">
+            <div>
+              <h4>SITE PLAN</h4>
+              <p>lorem ipsup lo rem epso</p>
+            </div>
+            <div class=" floorplants__optionMain">
+              <a href="#" class=" d-flex justify-content-between align-items-center">
+                <span class="mr-3">
+                  SHOW
+                </span>
+                <span class="floorplant__optionIcon">
+                  <svg width="20" height="29" viewBox="0 0 20 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 1L19 16.3187L1 35" stroke="black"/>
+                  </svg>
+                </span>
+              </a>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between">
+            <div>
+              <h4>TYPICAL UNIT PLAN</h4>
+              <p>lorem ipsup lo rem epso</p>
+            </div>
+            <div class=" floorplants__optionMain">
+              <a href="#" class=" d-flex justify-content-between align-items-center">
+                <span class="mr-3">
+                  SHOW
+                </span>
+                <span class="floorplant__optionIcon">
+                  <svg width="20" height="29" viewBox="0 0 20 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 1L19 16.3187L1 35" stroke="black"/>
+                  </svg>
+                </span>
+              </a>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
+
+      <div class="col-md-6 offset-1 floorplant__principalImage">
+        <div class="img-effect img-effect-solid image_background">
+          <img class="img-fluid" src="{{asset('image/floorplants_principal.jpg')}}" alt="">
+        </div>
+      </div>
+    </section>
 </div>
 @endsection
